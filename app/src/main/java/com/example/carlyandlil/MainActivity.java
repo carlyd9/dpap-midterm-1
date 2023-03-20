db8497ff000de3f4ce834b676f9fb333407929ea
Carly Diamond <carlydiamond@Carlys-MacBook-Air.local>
2023-03-20 19:51:37 +0200

    first commit

---
+++ app/src/main/java/com/example/carlyandlil/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.carlyandlil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.my_button);
+        btn.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show());
+
+    }
+}
