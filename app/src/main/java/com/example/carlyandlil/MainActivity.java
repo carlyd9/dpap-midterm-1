d291610b1eea08ca3e79018a5f5486eea504041a
carlyd9 <carly.diamond@deel.com>
2023-03-22 22:42:32 +0200

    commit carly

--- app/src/main/java/com/example/carlyandlil/MainActivity.java
+++ app/src/main/java/com/example/carlyandlil/MainActivity.java
@@ -6,27 +6,39 @@ package com.example.carlyandlil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
 
         Button btn = findViewById(R.id.my_button);
-        btn.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show());
-
-        Button btn10 = findViewById(R.id.my_button10);
-        btn10.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show());
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show();
+            }
+        });
         Button btn2 = findViewById(R.id.my_button2);
-        btn2.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show());
-
-
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show();
+            }
+        });
+        Button btn10 = findViewById(R.id.my_button10);
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
