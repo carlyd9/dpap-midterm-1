08cf452f5f022131cac03b31c98c51d44e6bd5f1
carlyd9 <carly.diamond@deel.com>
2023-03-22 22:31:08 +0200

    commit carly

--- app/src/main/java/com/example/carlyandlil/MainActivity.java
+++ app/src/main/java/com/example/carlyandlil/MainActivity.java
@@ -20,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.my_button);
         btn.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button Clicked!",Toast.LENGTH_LONG).show());
 
+        Button btn10 = findViewById(R.id.my_button10);
+        btn10.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button 10 Clicked!",Toast.LENGTH_LONG).show());
+
+        Button btn2 = findViewById(R.id.my_button2);
+        btn2.setOnClickListener(view -> Toast.makeText(getApplicationContext(),"Button 2 Clicked!",Toast.LENGTH_LONG).show());
+
+
+
     }
 }
 
