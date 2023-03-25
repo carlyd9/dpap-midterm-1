5ea8321c3471b5f365a75dc75e6b0d7432f025d2
carlyd9 <carly.diamond@deel.com>
2023-03-25 22:46:05 +0300

    commit carly

--- app/src/main/java/com/example/carlyandlil/Activity3.java
+++ app/src/main/java/com/example/carlyandlil/Activity3.java
@@ -1,6 +1,12 @@
 package com.example.carlyandlil;
 
+import static com.example.carlyandlil.R.id.my_button12;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +15,19 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3);
+
+        ImageButton btn502 = findViewById(R.id.my_button12);
+        btn502.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Lets see your new pic",Toast.LENGTH_LONG).show();
+                Intent homepage4 = new Intent(view.getContext(), Activity4.class);
+                startActivity(homepage4);
+            }
+        });
+
     }
+
 }
+
 
---
+++ app/src/main/java/com/example/carlyandlil/Activity4.java
@@ -0,0 +1,13 @@
+package com.example.carlyandlil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity4 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity4);
+    }
+}
