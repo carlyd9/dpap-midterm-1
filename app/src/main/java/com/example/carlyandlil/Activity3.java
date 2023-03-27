b53ea1610f4e7eb321bb561b6575001db32402d0
carlyd9 <carly.diamond@deel.com>
2023-03-27 17:38:04 +0300

    commit carly

--- app/src/main/java/com/example/carlyandlil/Activity3.java
+++ app/src/main/java/com/example/carlyandlil/Activity3.java
@@ -1,5 +1,6 @@
 package com.example.carlyandlil;
 
+import static com.example.carlyandlil.R.id.camerabutton5;
 import static com.example.carlyandlil.R.id.profileb;
 
 import android.content.Intent;
@@ -21,13 +22,37 @@ public class Activity3 extends AppCompatActivity {
         btn503.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Lets see your new pic",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Lets see your new pic", Toast.LENGTH_LONG).show();
                 Intent homepage4 = new Intent(view.getContext(), Activity4.class);
                 startActivity(homepage4);
             }
         });
-    }
 
+        ImageButton btnprofile = findViewById(profileb);
+        btnprofile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Take me to my profile", Toast.LENGTH_LONG).show();
+                Intent homepage5 = new Intent(view.getContext(), Activity4.class);
+                startActivity(homepage5);
+            }
+        });
+
+
+        ImageButton btncamera = findViewById(camerabutton5);
+        btncamera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "retake pic", Toast.LENGTH_LONG).show();
+                Intent homepage5 = new Intent(view.getContext(), Activity5.class);
+                startActivity(homepage5);
+            }
+        });
+
+    }
 }
+
 
 
